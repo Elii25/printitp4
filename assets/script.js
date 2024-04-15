e75a4703f58495f78dcc002ca527380a68a84e2f
El-Anziz ISHAKA <elanzizattou@gmail.com>
2024-04-15 17:33:58 +0400

    Ajouter des bullet points au slider

--- assets/script.js
+++ assets/script.js
@@ -48,3 +48,48 @@ for (let i= 0; i < slides.length; i++){
 	//Ajouter l'element au conteneur
 	dotsContaire.appendChild(newDot)
 }
+
+//Modifier le slide au clic sur un bouton
+document.addEventListener("DOMContentLoaded", function() {
+	const slides = document.querySelectorAll(".slides");
+	const prevBtn = document.getElementById("prevBtn");
+	const nextBtn = document.getElementById("nextBtn");
+	const tagLines = ["Impressions tous formats <span>en boutique et en ligne", "Tirages haute définition grand format <span>pour vos bureaux et events", "Grand choix de couleurs <span>de CMJN aux pantones", "Autocollants <span>avec découpe laser sur mesure"];
+	let currentSlide = 0;
+  
+	function showSlide(index) {
+	  slides.forEach((slide, i) => {
+		if (i === index) {
+		  slide.style.display = "block";
+		} else {
+		  slide.style.display = "none";
+		}
+	  });
+	}
+  
+	function changeSlide(direction) {
+	  if (direction === "next") {
+		currentSlide = (currentSlide + 1) % slides.length;
+	  } else {
+		currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+	  }
+	  const imagePath = "image" + (currentSlide + 1) + ".jpg"; // Construire le chemin de la nouvelle image
+	  const tagLine = tagLines[currentSlide];
+	  const currentImage = slides[currentSlide].querySelector("img");
+	  const tagLineElement = slides[currentSlide].querySelector("p");
+	  currentImage.src = imagePath; // Changer l'image
+	  tagLineElement.innerHTML = tagLine; // Insérer le texte
+	  showSlide(currentSlide);
+	}
+  
+	prevBtn.addEventListener("click", function() {
+	  changeSlide("prev");
+	});
+  
+	nextBtn.addEventListener("click", function() {
+	  changeSlide("next");
+	});
+  
+	showSlide(currentSlide);
+  });
+  
